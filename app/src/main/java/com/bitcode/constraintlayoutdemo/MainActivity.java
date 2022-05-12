a12f3b97281f05c9117e7332f39f9a3d018a9e18
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-05-12 12:37:25 +0530

    ConstraintLayout and Toast

---
+++ app/src/main/java/com/bitcode/constraintlayoutdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.bitcode.constraintlayoutdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Form Submitted..", Toast.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+}
